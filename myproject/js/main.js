dac55050de89f5b8c22d0a864b6d732bd713d327
qqLimbo <46644286+qqLimbo@users.noreply.github.com>
2019-01-25 13:07:46 +0200

    Add files via upload

---
+++ myproject/js/main.js
@@ -0,0 +1,19 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    Cars: [
+      { 
+      	mark: 'Lamborghini',
+      	detail:'Lambadvij' 
+      },
+      { 
+      	mark: 'Ferrari',
+      	detail:'Ferrbadvij' 
+      },
+      { 
+      	mark: 'Bugatti',
+      	detail:'Bugdvij' 
+      }
+    ]
+  }
+})
